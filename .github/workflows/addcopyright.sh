a57f1a40511bf2e9f651031aeb07a56942d0b21a
Klemen <klemenzmahar@gmail.com>
2021-12-09 22:19:31 +0100

    comit

--- .github/workflows/addcopyright.sh
+++ .github/workflows/addcopyright.sh
@@ -1,4 +1,7 @@
+# shellcheck disable=SC2048
 for x in $*; do
-head -$COPYRIGHTLEN $x | diff /home/runner/work/ViralManager/ViralManager/.github/workflows/copyright.txt - || ( ( cat /home/runner/work/ViralManager/ViralManager/.github/workflows/copyright.txt; echo; cat $x) > /tmp/file;
-mv /tmp/file $x )
+  if grep -q "$x" "$*"; then
+    head -$COPYRIGHTLEN $x | diff /home/runner/work/ViralManager/ViralManager/.github/workflows/copyright.txt - || ( ( cat /home/runner/work/ViralManager/ViralManager/.github/workflows/copyright.txt; echo; cat $x) > /tmp/file;
+    mv /tmp/file $x )
+  fi
 done
