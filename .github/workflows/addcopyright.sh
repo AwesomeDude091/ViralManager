2f377f55067320bd197cc62f68fc36ede5411af7
awesomedude091 <computer.cubegeek4life@gmail.com>
2021-12-09 17:24:00 +0100

    Attempt 3

--- .github/workflows/addcopyright.sh
+++ .github/workflows/addcopyright.sh
@@ -1,4 +1,4 @@
 for x in $*; do
-head -$COPYRIGHTLEN $x | diff .github/workflows/copyright.txt - || ( ( cat copyright.txt; echo; cat $x) > /tmp/file;
+head -$COPYRIGHTLEN $x | diff /home/runner/work/ViralManager/ViralManager/.github/workflows/copyright.txt - || ( ( cat /home/runner/work/ViralManager/ViralManager/.github/workflows/copyright.txt; echo; cat $x) > /tmp/file;
 mv /tmp/file $x )
 done
