6cf0a649d4db42666c1d5fb3ed9e0eda3287be38
AwesomeDude091 <58686898+AwesomeDude091@users.noreply.github.com>
2021-12-09 22:29:36 +0100

    Update addcopyright-html.sh

--- .github/workflows/addcopyright-html.sh
+++ .github/workflows/addcopyright-html.sh
@@ -1,6 +1,6 @@
 # shellcheck disable=SC2048
 for x in $*; do
-  if ! grep -q "$x" "/home/runner/work/ViralManager/ViralManager/.github/workflows/copyright.txt"; then
+  if ! grep -q "$x" "/home/runner/work/ViralManager/ViralManager/.github/workflows/copyright-html.txt"; then
     head -$COPYRIGHTLEN $x | diff /home/runner/work/ViralManager/ViralManager/.github/workflows/copyright-html.txt - || ( ( cat /home/runner/work/ViralManager/ViralManager/.github/workflows/copyright-html.txt; echo; cat $x) > /tmp/file;
     mv /tmp/file $x )
   fi
