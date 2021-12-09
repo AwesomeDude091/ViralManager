a0a3a5b38beacad59c178599ec12ecd3e01c928d
Klemen <klemenzmahar@gmail.com>
2021-12-09 22:23:18 +0100

    grep reversal so that it checks for missing textand not existing text

--- .github/workflows/addcopyright-html.sh
+++ .github/workflows/addcopyright-html.sh
@@ -1,6 +1,6 @@
 # shellcheck disable=SC2048
 for x in $*; do
-  if grep -q "$x" "$*"; then
+  if ! grep -q "$x" "$*"; then
     head -$COPYRIGHTLEN $x | diff /home/runner/work/ViralManager/ViralManager/.github/workflows/copyright-html.txt - || ( ( cat /home/runner/work/ViralManager/ViralManager/.github/workflows/copyright-html.txt; echo; cat $x) > /tmp/file;
     mv /tmp/file $x )
   fi
