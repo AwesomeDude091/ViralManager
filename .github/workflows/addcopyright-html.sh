2a21d55ecf751fb328a6aa1d8af2b6c020ce051e
Klemen <klemenzmahar@gmail.com>
2021-12-09 22:36:19 +0100

    comit

--- .github/workflows/addcopyright-html.sh
+++ .github/workflows/addcopyright-html.sh
@@ -1,6 +1,6 @@
 # shellcheck disable=SC2048
 for x in $*; do
-  if ! grep -q "$x" "$*"; then
+  if ! grep -q "Copyright © Viral Innovation - All Rights Reserved" "$x"; then
     head -$COPYRIGHTLEN $x | diff /home/runner/work/ViralManager/ViralManager/.github/workflows/copyright-html.txt - || ( ( cat /home/runner/work/ViralManager/ViralManager/.github/workflows/copyright-html.txt; echo; cat $x) > /tmp/file;
     mv /tmp/file $x )
   fi
