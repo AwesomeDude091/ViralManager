77339c9b72cd35af762338c5e9c29642fb794e3a
AwesomeDude091 <58686898+AwesomeDude091@users.noreply.github.com>
2021-12-09 22:26:49 +0100

    Update addcopyright.sh

--- .github/workflows/addcopyright.sh
+++ .github/workflows/addcopyright.sh
@@ -1,6 +1,6 @@
 # shellcheck disable=SC2048
 for x in $*; do
-  if ! grep -q "$x" "$*"; then
+  if ! grep -q "$x" "/home/runner/work/ViralManager/ViralManager/.github/workflows/copyright.txt"; then
     head -$COPYRIGHTLEN $x | diff /home/runner/work/ViralManager/ViralManager/.github/workflows/copyright.txt - || ( ( cat /home/runner/work/ViralManager/ViralManager/.github/workflows/copyright.txt; echo; cat $x) > /tmp/file;
     mv /tmp/file $x )
   fi
